43561951b4009c46db6c0f87ca39e2d2d1a4aa8e
yehtetaung <yh.a@mit.com.mm>
2024-09-29 10:12:10 +0630

    fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,14 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DrizzleModule } from './drizzle/drizzle.module';
+import { ConfigModule } from '@nestjs/config';
 
-describe('AppController', () => {
-  let controller: AppController;
-  let service: AppService;
-
-  const mockAppService = {
-    getAll: jest.fn().mockResolvedValue([{ id: 1, name: 'Test Product' }]), // Mocking getAll method
-  };
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [
-        {
-          provide: AppService,
-          useValue: mockAppService,
-        },
-      ],
-    }).compile();
-
-    controller = module.get<AppController>(AppController);
-    service = module.get<AppService>(AppService);
-  });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-
-  describe('getAll', () => {
-    it('should return the result from the AppService', async () => {
-      const result = await controller.getAll();
-      expect(result).toEqual([{ id: 1, name: 'Test Product' }]);
-      expect(service.getAll).toHaveBeenCalled();
-    });
-  });
-});
+@Module({
+  imports: [DrizzleModule,ConfigModule.forRoot({
+    envFilePath: process.env.NODE_ENV === 'production' ? '.env.prod':'.env',
+  })],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
